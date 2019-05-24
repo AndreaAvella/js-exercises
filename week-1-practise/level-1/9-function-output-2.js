44497dfd913799ed1fce0aa0c3ae4a8214ca4153
Andrea Avella <50200771+AndreaAvella@users.noreply.github.com>
2019-05-25 00:06:35 +0200

    Update 9-function-output-2.js

--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -6,9 +6,9 @@ var result = s('code', 'your');
 console.log(result);
 
 // Answer these questions:
-// - What does this program do? Run it and see, or Google some of the methods used ot understand them.
-// - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
-// - What does the function return?
+// - What does this program do? Run it and see, or Google some of the methods used ot understand them. //concatenation of words
+// - How many parameters does the function take? //2
+// - What is the function name? s
+// - Where is the function called? with what parameters? //s (w1, w2)
+// - What does the function return? //codeyour
 // - How can this function be improved?
