2cba4b4207364a192fe5e5218a34ee8da334134e
Andrea Avella <50200771+AndreaAvella@users.noreply.github.com>
2019-05-25 00:02:35 +0200

    Update 8-function-output-1.js

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -6,7 +6,7 @@ var result = getNumber();
 console.log(result);
 
 // Answer these questions:
-// - What does this program do?
+// - What does this program do? returns a random number between 0 and 1.
 // - How many parameters does the function take?
-// - What is the function name?
-// - Where is the function called? with what parameters?
+// - What is the function name? getNumber
+// - Where is the function called? with what parameters?function getNumber()
